9d14ab577d741214c15adbde21bf19733a8cb50d
Muhammad Irfan <Muhammad Irfan>
2016-05-13 14:21:39 +0700

    iteration 1

--- dist/javascripts/script.js
+++ dist/javascripts/script.js
@@ -1,44 +1,44 @@
-$(document).ready(function(){
-    $(".navButton").click(function(e) {
-        e.preventDefault();
+// $(document).ready(function(){
+//     $(".navButton").click(function(e) {
+//         e.preventDefault();
       
-        //close collapsed navbar
-        if($('.navbar-toggle').css('display') !='none'){
-          $(".navbar-toggle").trigger( "click" );
-        }
+//         //close collapsed navbar
+//         if($('.navbar-toggle').css('display') !='none'){
+//           $(".navbar-toggle").trigger( "click" );
+//         }
         
-        //animate to section
-        var _this = $(this);
-        $('html, body').animate({
-            scrollTop: $('#' + _this.data('section')).offset().top
-        }, 300);
-    });
+//         //animate to section
+//         var _this = $(this);
+//         $('html, body').animate({
+//             scrollTop: $('#' + _this.data('section')).offset().top
+//         }, 300);
+//     });
 
-    var images = [
-        "https://www.dropbox.com/s/dakyyuwenfk0hgx/officespace.jpg?raw=1",
-        "http://cdn.backgroundhost.com/backgrounds/subtlepatterns/60degree_gray.png",
-        "https://www.dropbox.com/s/nxqiies7po29u98/American-skyscrapers.jpg?raw=1",
-        "https://www.dropbox.com/s/po7sdzsifxi5pgx/laptop.jpg?raw=1",
-        "http://cdn.backgroundhost.com/backgrounds/subtlepatterns/retina_wood.png"
-    ];
-    var ready = 0;
+//     var images = [
+//         "https://www.dropbox.com/s/dakyyuwenfk0hgx/officespace.jpg?raw=1",
+//         "http://cdn.backgroundhost.com/backgrounds/subtlepatterns/60degree_gray.png",
+//         "https://www.dropbox.com/s/nxqiies7po29u98/American-skyscrapers.jpg?raw=1",
+//         "https://www.dropbox.com/s/po7sdzsifxi5pgx/laptop.jpg?raw=1",
+//         "http://cdn.backgroundhost.com/backgrounds/subtlepatterns/retina_wood.png"
+//     ];
+//     var ready = 0;
 
-    for (var i = 0; i < images.length; i++) {
-        var img = new Image();
-        img.src = images[i];
-        img.onload = function() {
-            ready++;
-            onImageReady();
-        }
-    }
+//     for (var i = 0; i < images.length; i++) {
+//         var img = new Image();
+//         img.src = images[i];
+//         img.onload = function() {
+//             ready++;
+//             onImageReady();
+//         }
+//     }
 
-    function onImageReady() {
-        if (ready < images.length) {
-            return;
-        }
+//     function onImageReady() {
+//         if (ready < images.length) {
+//             return;
+//         }
 
-        console.log('image ready');
-        $("#customModal").hide();
-        $("#content").fadeIn();
-    }
-});
+//         console.log('image ready');
+//         $("#customModal").hide();
+//         $("#content").fadeIn();
+//     }
+// });
---
+++ structure.md
@@ -0,0 +1,6 @@
+Beranda
+Tentang kami
+My Product
+Legalitas : SIUP, TDP, dll
+Rekanan
+Contact Us
